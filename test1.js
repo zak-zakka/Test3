b48b5d8ea37590409976f863276f84f1de2f35b7
Alex Zakamaldin <azakamaldin@gmail.com>
2022-10-26 09:47:06 +0300

    init: First commit add files index.html test1.js test2.js

---
+++ test1.js
@@ -0,0 +1,12 @@
+var now = new Date();
+var start = new Date(now.getFullYear(), 0, 0);
+var diff = now - start;
+var oneDay = 1000 * 60 * 60 * 24;
+var day = Math.floor(diff / oneDay);
+
+console.log('var_now: ' + now);
+console.log('var_start: ' + start);
+console.log('var_diff: ' + diff);
+console.log('var_oneDay: ' + oneDay);
+
+console.log('Day of year: ' + day);
